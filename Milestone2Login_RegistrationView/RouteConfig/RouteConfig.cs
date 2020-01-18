7bd5f130d1232389d9f8e78c21250bf8d5170907
ZSwoveland <zswoveland@my.gcu.edu>
2020-01-18 10:07:09 -0600

    Here is what I did for the milestone 2 project. If you do not like the color scheme we can always change it. I put the alter burndown with the folder and create other views to just have a place to go if they will want to login.

---
+++ Milestone2Login_RegistrationView/RouteConfig/RouteConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Milestone_Project_CST247
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{Login}",
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+                name: "Register",
+                url: "{Register}",
+                defaults: new { controller = "Register", action = "Index", id = UrlParameter.Optional });
+        
+        routes.MapRoute(
+             name: "About",
+                url: "{About}",
+                defaults: new { controller = "About",action = "Index", id = UrlParameter.Optional
+             });
+        
+                routes.MapRoute(
+                     name: "Help",
+                      url: "{Help}",
+                 defaults: new { controller = "Help",action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
