f7c8d995692310046b1c03c66aca0d38ad11ad7e
dpj7217 <40005241+dpj7217@users.noreply.github.com>
2020-01-17 22:16:14 -0800

    Add files via upload

---
+++ Milestone2ModelAndDAO/Services/DAO.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Milestone2.Services
+{
+    public class DAO
+    {
+        //supply SqlConnection object as class member variable
+        private readonly SqlConnection conn = new SqlConnection("data source=(localdb)\\MSSQLLocalDB; database=Test; integrated security = SSPI");
+
+
+         /*********************************
+         *                                *
+         *          Login DAO Method      *
+         *                                *  
+         *       To login User call       *
+         *      Login Business Method     *    
+         *                                * 
+         **********************************/
+        public bool Login(string Username, string Password)
+        {
+            //create new SqlCommand to SELECT user with username and password
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE Username = '" + Username + "' AND Password = '" + Password + "'", this.conn);
+
+            //open database connection
+            conn.Open();
+
+            //execute command
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //check to see if execution returned any rows
+            if (reader.HasRows)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+       /**********************************
+        *                                *
+        *      Register DAO Method       *
+        *                                *
+        *   Use with form generated User *  
+        *               obj              *
+        *                                *
+        *    To Register new User call   *
+        *     Register Business Method   *
+        *                                * 
+        **********************************/
+        public bool Register(Milestone2.Models.User user)
+        {
+            //create SqlCommand to insert new user into DB
+            SqlCommand cmd = new SqlCommand("INSERT INTO Users(FirstName, LastName, Username, Password) VALUES('" + user.FirstName + "', '" + user.LastName + "', '" + user.Username + "', '" + user.Password + "')", this.conn);
+
+            //open database connection
+            conn.Open();
+
+            //execute query
+            int rows = cmd.ExecuteNonQuery();
+
+            //check to see how many rows were affected
+            if (rows >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Milestone2ModelAndDAO/Services/DAObusiness.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Milestone2.Services
+{
+    public class DAObusiness
+    {
+
+        /**********************************
+         *                                *
+         *     Login Business Method      *
+         *                                *
+         *    Call this method to Login   * 
+         **********************************/
+        public bool Login(string Username, string Password)
+        {
+            DAO dao = new DAO();
+
+            return dao.Login(Username, Password);
+        }
+
+
+        /**********************************
+        *                                *
+        *          Register Method       *
+        *                                *
+        *   Use with form generated User *  
+        *             obj                *
+        *                                *
+        *  Call this method to Register  *
+        *           new User             * 
+        ********************************** */
+        public bool register(Milestone2.Models.User newUser)
+        {
+            DAO dao = new DAO();
+
+            return dao.Register(newUser);
+        }
+
+    }
+}
